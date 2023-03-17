797251c46e4218d84cf81223d5c03957a0edafc2
Cristian Rodrigo Estrada Romero <47439120+CristianEstrada@users.noreply.github.com>
2023-03-17 16:21:50 -0500

    Front Schema
    Adding example architecture

---
+++ src/data.js
@@ -0,0 +1,86 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://rockcontent.com/es/wp-content/uploads/sites/3/2019/02/o-que-e-produto-no-mix-de-marketing-1024x538.png.webp",
+        title: "VENTAS FLASH",
+        desc: "lorem ipsum dolor sit amet, consectetur",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://rockcontent.com/es/wp-content/uploads/sites/3/2019/02/o-que-e-produto-no-mix-de-marketing-1024x538.png.webp",
+        title: "VENTAS ",
+        desc: "lorem ipsum dolor sit amet, consectetur",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://rockcontent.com/es/wp-content/uploads/sites/3/2019/02/o-que-e-produto-no-mix-de-marketing-1024x538.png.webp",
+        title: "FLASH ",
+        desc: "lorem ipsum dolor sit amet, consectetur",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [
+    {
+        id:1,
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1024px-DC_Comics_logo.svg.png",
+        title: "DC COMICS",
+    },
+    {
+        id:2,
+        img: "https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg",
+        title: "MARVEL",
+    },
+    {
+        id:3,
+        img: "",
+        title: "OTROS",
+    },
+];
+
+export const popularProducts = [
+    {
+        id:1,
+        img: "https://hips.hearstapps.com/hmg-prod/images/marvel-gifts-index-image-1665410889.jpg?crop=0.502xw:1.00xh;0,0&resize=980:*",
+    },
+    {
+        id:2,
+        img: "https://hips.hearstapps.com/hmg-prod/images/marvel-gifts-index-image-1665410889.jpg?crop=0.502xw:1.00xh;0,0&resize=980:*",
+    },
+    {
+        id:3,
+        img: "https://hips.hearstapps.com/hmg-prod/images/marvel-gifts-index-image-1665410889.jpg?crop=0.502xw:1.00xh;0,0&resize=980:*",
+    },
+    {
+        id:4,
+        img: "https://hips.hearstapps.com/hmg-prod/images/marvel-gifts-index-image-1665410889.jpg?crop=0.502xw:1.00xh;0,0&resize=980:*",
+    },
+    {
+        id:5,
+        img: "https://hips.hearstapps.com/hmg-prod/images/marvel-gifts-index-image-1665410889.jpg?crop=0.502xw:1.00xh;0,0&resize=980:*",
+    },
+    {
+        id:6,
+        img: "https://hips.hearstapps.com/hmg-prod/images/marvel-gifts-index-image-1665410889.jpg?crop=0.502xw:1.00xh;0,0&resize=980:*",
+    },
+    {
+        id:7,
+        img: "https://hips.hearstapps.com/hmg-prod/images/marvel-gifts-index-image-1665410889.jpg?crop=0.502xw:1.00xh;0,0&resize=980:*",
+    },
+    {
+        id:8,
+        img: "https://hips.hearstapps.com/hmg-prod/images/marvel-gifts-index-image-1665410889.jpg?crop=0.502xw:1.00xh;0,0&resize=980:*",
+    },
+    {
+        id:9,
+        img: "https://hips.hearstapps.com/hmg-prod/images/marvel-gifts-index-image-1665410889.jpg?crop=0.502xw:1.00xh;0,0&resize=980:*",
+    },
+    {
+        id:10,
+        img: "https://hips.hearstapps.com/hmg-prod/images/marvel-gifts-index-image-1665410889.jpg?crop=0.502xw:1.00xh;0,0&resize=980:*",
+    },
+
+
+];
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Announcements from '../components/Announcements'
+import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+import Categories from '../components/Categories'
+import Products from '../components/Products'
+
+const Home = () => {
+  return (
+    <div>
+      <Announcements/>
+      <Navbar/>
+      <Slider/>
+      <Categories/>
+      <Products/>
+    </div>
+  )
+}
+
+export default Home
