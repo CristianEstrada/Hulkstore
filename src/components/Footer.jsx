c15bf0e05f62c848943ee4d5db645d3104a0dfd1
Cristian Rodrigo Estrada Romero <47439120+CristianEstrada@users.noreply.github.com>
2023-03-17 16:43:55 -0500

    update
    hotfix animations and styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    display: flex;
+`
+const Left = styled.div``
+const Center = styled.div``
+const Right = styled.div``
+
+function Footer() {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default Footer
