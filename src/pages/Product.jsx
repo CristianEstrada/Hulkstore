a7147e632639c3c3b213f967cc2c14df8ced0005
Cristian Rodrigo Estrada Romero <47439120+CristianEstrada@users.noreply.github.com>
2023-03-17 22:20:11 -0500

    Responsive update

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,11 +3,13 @@ import styled from "styled-components";
 import Announcements from "../components/Announcements";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import {mobile} from "../responsive"
 
 const Container = styled.div``;
 const Wrapper = styled.div`
     padding: 50px;
     display: flex;
+    ${mobile({padding: "10px", flexDirection: "column"})}
 `;
 const ImgContainer = styled.div`
     flex: 1;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Announcements from "../components/Announcements";
 import Products from "../components/Products";
 import Footer from "../components/Footer";
+import {mobile} from "../responsive"
 
 const Container = styled.div``;
 const Title = styled.h1`
@@ -14,15 +15,18 @@ const FilterContainer = styled.div`
 `;
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({width: "0px 20px", display: "flex", flexDirection: "column"})}
 `;
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+  ${mobile({marginRight: "0"})}
 `;
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
+  ${mobile({margin: "10px 0px"})}
 `;
 const Option = styled.option``;
 
