2e4ebbcd6f58d0f244118479447fa6c911aedf3c
Cristian Rodrigo Estrada Romero <47439120+CristianEstrada@users.noreply.github.com>
2023-03-19 23:29:14 -0500

    update

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MTU1MDI0ZDc0MGU3MTJlZDNlN2FjOCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3OTI1ODg2OCwiZXhwIjoxNjc5NTE4MDY4fQ.gUmzPJG0AHPQqbKFmXG6I_E65aIA_18VGO_-zgSZSO0";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user || "{}")?.currentUser?.accessToken;
+
+//JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+  //.currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -10,5 +12,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  headers: { token: `Bearer ${TOKEN}` },
 });
